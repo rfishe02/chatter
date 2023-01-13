fbefb5125af833f86f369ae80e7a91bfb2d37ac9
rfishe02 <fisher.renae@gmail.com>
2023-01-13 06:50:27 -0600

    Completed conversion service and return converted file to browser.

--- webapp/routes/convert.js
+++ webapp/routes/convert.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const multer  = require('multer')
+const multer  = require('multer');
 
+// We're using in memory storage to lessen conversion time. If the system recives large files, this could be an issue.
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
@@ -16,15 +17,28 @@ router.post('/', upload.single('recording'), async function(req, res, next) {
         return res.status(400).send({'message':'No files received'});
     }
 
-    const fileBuffer = fileUpload.buffer;
+    if(!ffmpeg.isLoaded()) {
+        await ffmpeg.load();
+    }
 
-    await ffmpeg.load();
-    ffmpeg.FS('writeFile', 'recording.webm', fileBuffer);
-    //await ffmpeg.run('-i', 'recording.webm', 'recording.mp3');
-    //await fs.promises.writeFile('./recording.mp3', ffmpeg.FS('readFile', 'recording.mp3'));
+    const webmBuffer = fileUpload.buffer; // Buffer type
+    ffmpeg.FS('writeFile', './recording.webm', webmBuffer);
+    await ffmpeg.run('-i', './recording.webm', './recording.mp3');
+    const mp3Array = ffmpeg.FS('readFile', './recording.mp3'); // Uint8Array type
+    const mp3Buffer = Buffer.from(mp3Array, 'binary'); // Need to wrap in Buffer to ensure Blob conversion in the browser.
 
-    return res.status(200).send({'message':'Received POST'});
+    res.set('Content-Type', 'audio/mp3');
+    return res.status(200).send(mp3Buffer); 
 
 });
+
+// May not be necessary to use since Buffer might be backed by a Uint8Array.
+function toUint8Array(buffer) {
+    const view = new Uint8Array(buffer.length);
+    for (let i = 0; i < buffer.length; i++) {
+        view[i] = buffer[i];
+    }
+    return view;
+}
 
 module.exports = router;
