5d38c67d5a704996cf3dbecbeb83fa14283923ee
rfishe02 <fisher.renae@gmail.com>
2024-01-27 14:45:19 -0600

    Updated requirements, cleaned up code.

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,6 +1,5 @@
 import io
 import base64
-import uuid
 import json
 
 # Import libraries for Whisper speech to text.
@@ -57,28 +56,19 @@ def reply():
 
     conversation = json.loads(request.form.get('conversation'))
     conversation.append(asr)
-
     start_index = max(0, len(conversation) - 3)
     conversation_string = '\n'.join(conversation[start_index:])
+
     txt_reply = generate_text_reply(conversation_string)
 
+    # Create audio from generated text.
     tts_wav, tts_rate = text_to_audio(txt_reply)
 
     # Get wav file and encode to base64 since we're sending it over json along with text data.
-    audio_buffer = io.BytesIO()
-    tts_wav_2d_tensor = tts_wav.reshape(1,tts_wav.size()[0])
-    torchaudio.save(audio_buffer, tts_wav_2d_tensor, tts_rate, format="wav")
-    audio_buffer.seek(0)
-    base64_tts_reply = base64.b64encode(audio_buffer.read()).decode('ASCII')
-    audio_buffer.close()
+    base64_tts_reply = encode_audio_base64(tts_wav, tts_rate)
 
     # Perform sentiment analysis.
-    sentiment_dict = vader.polarity_scores(txt_reply)
-    reply_sentiment = 'neutral'
-    if sentiment_dict['compound'] >= 0.05 :
-        reply_sentiment = 'positive'
-    elif sentiment_dict['compound'] <= - 0.05 :
-        reply_sentiment = 'negative'
+    reply_sentiment = analyze_sentiment(txt_reply)
 
     output["txt_asr"] = asr 
     output["txt_reply"] = txt_reply 
@@ -116,18 +106,51 @@ def audio_to_text(audio_file):
     return whisper_transcription
 
 def generate_text_reply(conversation):
+    reply_text = ''
 
-    inputs = chat_tokenizer([conversation], return_tensors="pt")
-    reply_ids = chat_model.generate(**inputs)
-    reply_text = chat_tokenizer.batch_decode(reply_ids, skip_special_tokens=True)
-    return reply_text[0]
+    if len(conversation) > 0:
+        inputs = chat_tokenizer([conversation], return_tensors="pt")
+        reply_ids = chat_model.generate(**inputs)
+        gen_reply_text = chat_tokenizer.batch_decode(reply_ids, skip_special_tokens=True)
+
+        reply_text = gen_reply_text[0]
+    
+    return reply_text
 
 def text_to_audio(generated_reply):
+    tts_wav, tts_rate = None, 0
 
-    tts_sample = TTSHubInterface.get_model_input(tts_task, generated_reply)
-    tts_wav, tts_rate = TTSHubInterface.get_prediction(tts_task, tts_model, tts_generator, tts_sample)
+    if len(generated_reply) > 0:
+        tts_sample = TTSHubInterface.get_model_input(tts_task, generated_reply)
+        tts_wav, tts_rate = TTSHubInterface.get_prediction(tts_task, tts_model, tts_generator, tts_sample)
 
     return tts_wav, tts_rate
+
+def analyze_sentiment(txt_reply):
+    reply_sentiment = 'neutral'
+    
+    if len(txt_reply) > 0:
+        sentiment_dict = vader.polarity_scores(txt_reply)
+    
+        if sentiment_dict['compound'] >= 0.05 :
+            reply_sentiment = 'positive'
+        elif sentiment_dict['compound'] <= - 0.05 :
+            reply_sentiment = 'negative'
+
+    return reply_sentiment
+
+def encode_audio_base64(tts_wav, tts_rate):
+    base64_tts_reply = ''
+
+    if tts_wav is not None:
+        audio_buffer = io.BytesIO()
+        tts_wav_2d_tensor = tts_wav.reshape(1,tts_wav.size()[0])
+        torchaudio.save(audio_buffer, tts_wav_2d_tensor, tts_rate, format="wav")
+        audio_buffer.seek(0)
+        base64_tts_reply = base64.b64encode(audio_buffer.read()).decode('ASCII')
+        audio_buffer.close()
+
+    return base64_tts_reply
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5050, debug=True)
